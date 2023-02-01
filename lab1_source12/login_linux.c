9dd4fe1581f47f125ff7c87197881552f56c8b28
olaussonkarl <54595917+olaussonkarl@users.noreply.github.com>
2023-02-01 16:14:36 +0100

    Add files via upload

---
+++ lab1_source12/login_linux.c
@@ -0,0 +1,121 @@
+/* $Header: https://svn.ita.chalmers.se/repos/security/edu/course/computer_security/trunk/lab/login_linux/login_linux.c 585 2013-01-19 10:31:04Z pk@CHALMERS.SE $ */
+
+/* gcc -std=gnu99 -Wall -g -o mylogin login_linux.c -lcrypt */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdio_ext.h>
+#include <string.h>
+#include <signal.h>
+#include <pwd.h>
+#include <sys/types.h>
+#include <crypt.h>
+/* Uncomment next line in step 2 */
+#include "pwent.h"
+
+#define TRUE 1
+#define FALSE 0
+#define LENGTH 16
+
+void sighandler() {
+	signal(SIGINT, SIG_IGN);
+	signal(SIGTSTP, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+	printf("cant kill the programm with crtl c, ctrl z or ctrl backslash!");
+}
+
+
+int main(int argc, char *argv[]) {
+
+	mypwent *passwddata; /* this has to be redefined in step 2 */
+	/* see pwent.h */
+
+	char important1[LENGTH] = "**IMPORTANT 1**";
+
+	char user[LENGTH];
+
+	char important2[LENGTH] = "**IMPORTANT 2**";
+
+	//char   *c_pass; //you might want to use this variable later...
+	char prompt[] = "password: ";
+	char *user_pass;
+
+	sighandler();
+
+
+	while (TRUE) {
+
+
+		/* check what important variable contains - do not remove, part of buffer overflow test */
+		printf("Value of variable 'important1' before input of login name: %s\n",
+				important1);
+		printf("Value of variable 'important2' before input of login name: %s\n",
+				important2);
+
+		printf("login: ");
+		fflush(NULL); /* Flush all  output buffers */
+		__fpurge(stdin); /* Purge any data in stdin buffer */
+
+
+		if (fgets(user, LENGTH, stdin) == NULL){ /* gets() is vulnerable to buffer */
+			exit(0); /*  overflow attacks.  */
+		}
+		user[strlen(user)-1]='\0';
+				
+		/* check to see if important variable is intact after input of login name - do not remove */
+		printf("Value of variable 'important 1' after input of login name: %*.*s\n",
+				LENGTH - 1, LENGTH - 1, important1);
+		printf("Value of variable 'important 2' after input of login name: %*.*s\n",
+		 		LENGTH - 1, LENGTH - 1, important2);
+
+		user_pass = getpass(prompt);
+		passwddata = mygetpwnam(user);
+
+		
+		if (passwddata != NULL) {
+			/* You have to encrypt user_pass for this to work */
+			/* Don't forget to include the salt */
+			char *encrypted_password = crypt(user_pass, passwddata->passwd_salt);	
+			
+			if(passwddata->pwfailed > 10)
+				sleep(10);
+				
+			if (!strcmp(passwddata->passwd, encrypted_password)) {
+
+				printf(" You're in !\n");
+				printf("%d\n", passwddata->pwfailed);
+				
+				/*increments of varibles*/
+				passwddata->pwage++;
+				passwddata->pwfailed=0;
+
+				/*tests*/	
+				if(mysetpwent(user, passwddata)== -1)
+					exit(0);
+
+				if(passwddata->pwage > 10)	
+					printf("the password is old, change it plz!\n");
+
+			
+				if(setuid(passwddata->uid) == -1){
+					perror("setuid");
+					exit(1);
+				}
+
+				char *const argv[] = {"/bin/sh", NULL};
+				
+				if(execve("/bin/sh",argv, NULL) == -1){
+					exit(2);
+				}
+
+			}else {
+				passwddata->pwfailed++;	
+				if(mysetpwent(user, passwddata) == -1)
+					exit(0);	
+			}
+		}
+		printf("Login Incorrect \n");
+	}
+	return 0;
+}
